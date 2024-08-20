07512ae16df81adc28e3db08c9fe956010f98f83
Corinne Mainardi <mainardi.corinne@gmail.com>
2024-08-20 16:31:59 +0200

    esercizi complet

--- D2.js
+++ D2.js
@@ -108,9 +108,14 @@ let b = 107;
 let c = 3;
 
 if (a < b && a > c && b > c) {
+  //107 54 3  maggiore >   --- minore <
   console.log(b, a, c);
 } else if (a > b && a > c && b < c) {
-  console.log(a, b, c);
+  console.log(a, c, b);
+} else if (a < b && a < c && b < c) {
+  console.log(c, b, a);
+} else if (a > b && a < c && b < c) {
+  console.log(c, a, b);
 } else {
   console.log("non sono in grado di ordinare i tuoi numeri");
 }
